bc3bb54f59710474439a6362c3148b4db1233c53
jrichards-dev <joshuarichards777@gmail.com>
2025-04-29 10:13:13 -0700

    added get_book_text() function

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    get_book_text("books/frankenstein.txt")
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+
+
+main()
