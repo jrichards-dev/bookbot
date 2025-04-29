d6afba740b96e00f2fc0e2caf1184b76b2af23b9
jrichards-dev <joshuarichards777@gmail.com>
2025-04-29 13:23:04 -0700

    added 'sort_dictionary' and printed full report in main

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from stats import get_num_words
 from stats import get_num_char
+from stats import sort_dictionary
 
 def main():
-    get_num_words(get_book_text("books/frankenstein.txt"))
-    get_num_char(get_book_text("books/frankenstein.txt"))
+    book = "books/frankenstein.txt"
+    word_count = []
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book}")
+
+    print("----------- Word Count ----------")
+    get_num_words(get_book_text(book))
+
+    print("--------- Character Count -------")
+    word_count = sort_dictionary(get_num_char(get_book_text(book)))
+    for word in word_count:
+        print(f"{word["char"]}: {word["num"]}")
+        
+    print("============= END ===============")
 
 
 def get_book_text(path_to_file):
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def get_num_words(text):
     word_list = []
     word_list = text.split()
     num_words = len(word_list)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
 
 def get_num_char(text):
     char_count = {}
@@ -12,5 +12,17 @@ def get_num_char(text):
         for i in range(0, len(word)):
             char_count[word[i]] = char_count.get(word[i], 0) + 1
 
-    for key in char_count:
-        print(f"'{key}': {char_count[key]}")
+    return char_count
+    #for key in char_count:
+    #   print(f"'{key}': {char_count[key]}")
+
+
+def sort_on(dict):
+    return dict["num"]
+
+def sort_dictionary(dictionary):
+    dict_list = []
+    for key in dictionary:
+        dict_list.append({"char": key, "num": dictionary[key]})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
