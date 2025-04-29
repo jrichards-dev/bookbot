83a6813d74095d30dcf4b295e16340a22dff36c4
jrichards-dev <joshuarichards777@gmail.com>
2025-04-29 10:48:42 -0700

    added 'get_num_char()' to stats.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from stats import get_num_words
+from stats import get_num_char
 
 def main():
     get_num_words(get_book_text("books/frankenstein.txt"))
+    get_num_char(get_book_text("books/frankenstein.txt"))
 
 
 def get_book_text(path_to_file):
--- stats.py
+++ stats.py
@@ -3,3 +3,14 @@ def get_num_words(text):
     word_list = text.split()
     num_words = len(word_list)
     print(f"{num_words} words found in the document")
+
+def get_num_char(text):
+    char_count = {}
+    text = text.lower()
+    word_list = text.split()
+    for word in word_list:
+        for i in range(0, len(word)):
+            char_count[word[i]] = char_count.get(word[i], 0) + 1
+
+    for key in char_count:
+        print(f"'{key}': {char_count[key]}")
